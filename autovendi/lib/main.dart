341766446c86adf86acbbd5359b6b1db4e589a01
M Hamza Tariq <111647708+Hmmza-tariq@users.noreply.github.com>
2024-01-30 11:48:03 +0500

    Authenticating with credentials

--- autovendi/lib/main.dart
+++ autovendi/lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:autovendi/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(LoginScreen());
 }
 
 class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    Color customGray = Color(0xFFA9A9A9);
+    Color customGray = const Color(0xFFA9A9A9);
     return MaterialApp(
       home: Scaffold(
         body: Stack(
@@ -26,38 +37,39 @@ class LoginScreen extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
+                  const Text(
                     'A.U.T.O.V.E.N.D.I.',
                     style: TextStyle(fontSize: 35, color: Colors.black),
                   ),
-                  SizedBox(height: 20),
-                  Text(
+                  const SizedBox(height: 20),
+                  const Text(
                     'Welcome to Dynamic food delivery',
                     style: TextStyle(fontSize: 15, color: Colors.black),
                   ),
-                  SizedBox(height: 20),
-                  Container(
+                  const SizedBox(height: 20),
+                  SizedBox(
                     width: 300, // Set the desired width
                     child: ElevatedButton.icon(
                       onPressed: () {
                         // Handle Google sign-in
                       },
-                      icon: Icon(Icons.mail),
-                      label: Text('Sign in with Google'),
+                      icon: const Icon(Icons.mail),
+                      label: const Text('Sign in with Google'),
                       style: ElevatedButton.styleFrom(
                           backgroundColor:
                               const Color.fromARGB(255, 195, 32, 20)),
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Container(
+                  const SizedBox(height: 10),
+                  SizedBox(
                     width: 300, // Set the desired width
                     child: ElevatedButton.icon(
                       onPressed: () {
-                        // Handle email sign-in
+                        signUpUser(
+                            emailController.text, passwordController.text);
                       },
-                      icon: Icon(Icons.email),
-                      label: Text(
+                      icon: const Icon(Icons.email),
+                      label: const Text(
                         'Sign up with Email',
                         style: TextStyle(color: Colors.black),
                       ),
@@ -67,10 +79,11 @@ class LoginScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Container(
+                  const SizedBox(height: 20),
+                  SizedBox(
                     width: 300, // Set your desired width
                     child: TextFormField(
+                      controller: emailController,
                       decoration: InputDecoration(
                         labelText: 'Email',
                         border: OutlineInputBorder(
@@ -79,10 +92,11 @@ class LoginScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Container(
+                  const SizedBox(height: 10),
+                  SizedBox(
                     width: 300, // Set your desired width
                     child: TextFormField(
+                      controller: passwordController,
                       decoration: InputDecoration(
                         labelText: 'Password',
                         border: OutlineInputBorder(
@@ -91,30 +105,32 @@ class LoginScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   TextButton(
                     onPressed: () {
                       // Handle "Forgot your password?" action
                     },
-                    child: Text(
+                    child: const Text(
                       'Forgot your password?',
                       style: TextStyle(color: Colors.black),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Container(
+                  const SizedBox(height: 20),
+                  SizedBox(
                     width: 150, // Set the desired width
                     child: ElevatedButton(
                       onPressed: () {
-                        // Handle login
+                        // Handle email sign-in
+                        signUpUser(
+                            emailController.text, passwordController.text);
                       },
-                      child: Text(
-                        'Log In',
-                        style: TextStyle(color: Colors.black),
-                      ),
                       style: ElevatedButton.styleFrom(
                         backgroundColor: customGray, // Use custom gray color
                         // minimumSize: Size(150, 30),
+                      ),
+                      child: const Text(
+                        'Log In',
+                        style: TextStyle(color: Colors.black),
                       ),
                     ),
                   ),
@@ -125,5 +141,32 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<String?> signUpUser(String email, String password) async {
+    try {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return null;
+    } catch (e) {
+      if (kDebugMode) {
+        print('error: ${e.toString()}');
+      }
+      return "Error";
+    }
+  }
+
+  Future<String?> loginUser(String email, String password) async {
+    try {
+      FirebaseAuth auth = FirebaseAuth.instance;
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } catch (e) {
+      if (kDebugMode) {
+        print('error: ${e.toString()}');
+      }
+      return "Error";
+    }
   }
 }
